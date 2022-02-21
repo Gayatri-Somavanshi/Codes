cf2b95d9e6f7bc0aeb9d8c8bae8d295c199767d1
Gayatri-Somavanshi <88457416+Gayatri-Somavanshi@users.noreply.github.com>
2022-02-21 11:08:46 +0530

    Add files via upload

---
+++ Load2.ino.ino
@@ -0,0 +1,56 @@
+
+  #define DisplaySerial Serial
+
+
+
+
+#include "Picaso_Serial_4DLib.h"
+#include "BigDemo.h" 
+#include "Picaso_Const4D.h"
+
+Picaso_Serial_4DLib Display(&DisplaySerial);
+
+void setup() {
+  Display.TimeLimit4D   = 50 ;
+  DisplaySerial.begin(9600) ;
+  
+ Display.gfx_Cls(); 
+ Display.gfx_BGcolour(WHITE) ;
+ Display.gfx_ScreenMode(PORTRAIT);  
+   int i ;
+  int k ;
+  int l ;
+  k = 120 ;
+  l = 120 ;
+  word orbitx ;
+  word orbity ;
+  Display.gfx_MoveTo(k, l);
+  Display.gfx_CircleFilled(k,l,5,WHITE) ;
+  i = -90;   // 12 o'clock position  
+ Display.gfx_Cls(); 
+ Display.gfx_BGcolour(WHITE) ;
+ Display.gfx_ScreenMode(PORTRAIT);  
+
+ for(i=0;i<360;i++)
+{
+  Display.gfx_Orbit(i, 117, &orbitx, &orbity);
+   k = 5;
+   Display.gfx_CircleFilled(orbitx , orbity, k, RED);
+}
+for(i=0;i<360;i--)
+{
+  Display.gfx_Orbit(i, 117, &orbitx, &orbity);
+   k = 5;
+   Display.gfx_CircleFilled(orbitx , orbity, k, WHITE);
+}
+
+}
+  
+void loop(void)
+{
+  
+delay(50);
+
+ 
+}
+
